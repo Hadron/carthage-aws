971014a542d85e6e3879335229a2c2d58bbae452
Sam Hartman <hartmans@debian.org>
2022-03-01 16:48:49 -0700

    Make sure not to select default security group
    Long term this is wrong, but for now never select the default security
    group.

--- carthage_aws/network.py
+++ carthage_aws/network.py
@@ -95,7 +95,7 @@ class AwsVirtualPrivateCloud(AwsManaged):
         groups =self.connection.client.describe_security_groups(Filters=[
             dict(Name='vpc-id', Values=[self.id])])
 
-        self.groups = groups['SecurityGroups']
+        self.groups = list(filter(lambda g: g['GroupName'] != "default", groups['SecurityGroups']))
         
         
     def delete(self):
