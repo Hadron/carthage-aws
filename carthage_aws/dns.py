75ef217fa2bca88e5a9ffce24b1b944ddcfeb58a
Sam Hartman <hartmans@debian.org>
2022-03-03 13:34:46 -0700

    Add license to dns.py
    Covered by copyright assignment between Spencer Rak and Hadron Industries.

--- carthage_aws/dns.py
+++ carthage_aws/dns.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2022, Hadron Industries, Inc.
+# Carthage is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation. It is distributed
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
+# LICENSE for details.
 from carthage import *
 from carthage.dependency_injection import *
 from carthage.network import NetworkLink
