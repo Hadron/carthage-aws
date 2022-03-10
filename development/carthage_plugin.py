b6cb556b0f29750764e19ef30171dee23f13fc15
Sam Hartman <hartmans@debian.org>
2022-03-10 15:12:19 -0700

    Initial development layout
    Create an initial layout to install a development VM in AWS.  Intended
    to initially facilitate development of carthage_aws.image, but also
    generally useful for deploying an environment in which to test
    Carthage.

---
+++ development/carthage_plugin.py
@@ -0,0 +1,14 @@
+from carthage import Injector, inject, InjectionKey
+from carthage.config import ConfigSchema
+from carthage.modeling import CarthageLayout
+
+class DevelopmentSchema(ConfigSchema, prefix="developer"):
+    #: A route53 zone in which to register developer resources
+    
+    domain: str#: Name of primary developer vm
+    machine: str
+
+@inject(injector=Injector)
+def carthage_plugin(injector):
+    from layout import dev_layout
+    injector.add_provider(InjectionKey(CarthageLayout, layout_name='aws_development'), dev_layout)
