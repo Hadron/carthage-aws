aecd39b774ff29322b384937849093bdb856c30e
Klee Dienes <klee.dienes@hadronindustries.com>
2022-03-27 12:48:55 -0400

    RAM made a share

--- carthage_aws/ram.py
+++ carthage_aws/ram.py
@@ -3,42 +3,35 @@ import asyncio
 from carthage import *
 from carthage.dependency_injection import *
 from carthage.utils import memoproperty
-from carthage_aws.connection import AwsConnection, AwsManaged, run_in_executor
+from carthage_aws.connection import AwsConnection, AwsClientManaged, run_in_executor
 from carthage_aws.network import AwsVirtualPrivateCloud, AwsSubnet
 
 from dataclasses import dataclass, field
 
+from .utils import unpack
+
 from botocore.exceptions import ClientError
 
-class AwsResourceAccessManager(AwsManaged):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+__all__ = ['AwsResourceShare']
 
-    # accept_resource_share_invitation()
-    # associate_resource_share()
-    # associate_resource_share_permission()
-    # can_paginate()
-    # create_resource_share()
-    # delete_resource_share()
-    # disassociate_resource_share()
-    # disassociate_resource_share_permission()
-    # enable_sharing_with_aws_organization()
-    # get_paginator()
-    # get_permission()
-    # get_resource_policies()
-    # get_resource_share_associations()
-    # get_resource_share_invitations()
-    # get_resource_shares()
-    # get_waiter()
-    # list_pending_invitation_resources()
-    # list_permission_versions()
-    # list_permissions()
-    # list_principals()
-    # list_resource_share_permissions()
-    # list_resource_types()
-    # list_resources()
-    # promote_resource_share_created_from_policy()
-    # reject_resource_share_invitation()
-    # tag_resource()
-    # untag_resource()
-    # update_resource_share()
+class AwsResourceShare(AwsClientManaged):
+    def __init__(self, **kwargs):
+        if 'share' in kwargs.keys():    
+            self.share = kwargs.pop('share')
+        super().__init__(**kwargs)
+        self.arn = None
+
+    resource_type = 'resource_share'
+    client_type = 'ram'
+
+    def do_create(self):
+        r = self.client.create_resource_share(
+            name=self.name,
+            resourceArns=[self.share],
+            principals=['arn:aws-us-gov:organizations::807241311045:organization/o-yy8ngalms6'],
+            tags=[dict(key='Name',value=self.name)]
+        )
+        r = r['resourceShare']
+        self.cache = unpack(r)
+        self.arn = self.cache.resourceShareArn
+        return self.cache
