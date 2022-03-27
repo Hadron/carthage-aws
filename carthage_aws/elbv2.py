6b1ab1172ca5716172e32106e81c96dc9d289029
Klee Dienes <klee.dienes@hadronindustries.com>
2022-03-27 12:48:38 -0400

    Adding Listener and Targets

--- carthage_aws/elbv2.py
+++ carthage_aws/elbv2.py
@@ -3,14 +3,14 @@ import asyncio
 from carthage import *
 from carthage.dependency_injection import *
 from carthage.utils import memoproperty
-from carthage_aws.connection import AwsConnection, AwsManaged, AwsClientManaged, run_in_executor
+from carthage_aws.connection import AwsConnection, AwsManaged, AwsClientManaged, callback, run_in_executor
 from carthage_aws.network import AwsVirtualPrivateCloud, AwsSubnet
 
 from .utils import unpack
 
 from botocore.exceptions import ClientError
 
-class AwsGatewayLoadBalancer(AwsClientManaged):
+class AwsLoadBalancer(AwsClientManaged):
     def __init__(self, **kwargs):
         if ('subnet' in kwargs) and ('subnets' in kwargs):
             raise ValueError(f"call to AwsGatewayLoadBalancer should not specify both 'subnet' and 'subnets'")
@@ -22,28 +22,22 @@ class AwsGatewayLoadBalancer(AwsClientManaged):
             self.subnets = False
         super().__init__(**kwargs)
 
-    resource_type = 'gateway_load_balancer'
+        self.arn = None
+
+    resource_type = 'load_balancer'
     client_type = 'elbv2'
 
-    @memoproperty
-    def resource_name(self):
-        return "".join([x.title() for x in self.resource_type.split('_')])
-
-    @memoproperty
-    def client(self):
-        return self.connection.connection.client(self.client_type, self.connection.region)
-
     def do_create(self):
-        r = create_load_balancer(
-            Subnets=[
-                self.subnet,
-            ],
-            Scheme='internal',
-            Tags=[self.resource_tags]
+        r = self.client.create_load_balancer(
+            Name=self.name,
+            Subnets=[x.id for x in self.subnets],
+            Tags=[dict(Key='Name',Value=self.name)],
+            Type='gateway',
+            IpAddressType='ipv4'
         )
         r = r['LoadBalancers'][0]
         self.cache = unpack(r)
-        breakpoint()
+        self.arn = self.cache.LoadBalancerArn
         return self.cache
 
     def set_subnets(self, *args):
@@ -55,41 +49,88 @@ class AwsGatewayLoadBalancer(AwsClientManaged):
             Subnets=[x.id for x in args],
             IpAddressType='ipv4'
         )
-        breakpoint()
 
-    # add_listener_certificates()
-    # add_tags()
-    # can_paginate()
-    # create_listener()
-    # create_rule()
-    # create_target_group()
-    # delete_listener()
-    # delete_load_balancer()
-    # delete_rule()
-    # delete_target_group()
-    # deregister_targets()
-    # describe_account_limits()
-    # describe_listener_certificates()
-    # describe_listeners()
-    # describe_load_balancer_attributes()
-    # describe_load_balancers()
-    # describe_rules()
-    # describe_ssl_policies()
-    # describe_tags()
-    # describe_target_group_attributes()
-    # describe_target_groups()
-    # describe_target_health()
-    # get_paginator()
-    # get_waiter()
-    # modify_listener()
-    # modify_load_balancer_attributes()
-    # modify_rule()
-    # modify_target_group()
-    # modify_target_group_attributes()
-    # register_targets()
-    # remove_listener_certificates()
-    # remove_tags()
-    # set_ip_address_type()
-    # set_rule_priorities()
-    # set_security_groups()
-    # set_subnets()
+    @callback
+    def delete(self):
+        self.client.delete_load_balancer(LoadBalancerArn=self.cache.LoadBalancerArn)
+
+@inject_autokwargs(vpc=AwsVirtualPrivateCloud)
+class AwsLoadBalancerTargetGroup(AwsClientManaged):
+    def __init__(self, **kwargs):
+        if ('target' in kwargs) and ('targets' in kwargs):
+            raise ValueError(f"call to AwsGatewayLoadBalancer should not specify both 'target' and 'targets'")
+        elif ('target' in kwargs):
+            self.targets = [kwargs.pop('target')]
+        elif ('targets' in kwargs):
+            self.targets = kwargs.pop('targets')
+        else:
+            self.subnets = False
+        super().__init__(**kwargs)
+        self.arn = None
+
+    resource_type = 'target_group'
+    client_type = 'elbv2'
+    allproto = ['HTTP'|'HTTPS'|'TCP'|'TLS'|'UDP'|'TCP_UDP'|'GENEVE']
+
+    def do_create(self):
+        assert self.proto in allproto,f"{self.proto} must be one of {allproto}"
+        response = client.create_target_group(
+            Name=self.name,
+            Protocol='GENEVE',
+            ProtocolVersion='string',
+            Port=6081,
+            VpcId=self.vpc,
+            TargetType='instance',
+            Tags=[dict(Key='Name',Value=self.name)],
+            IpAddressType='ipv4'
+        )
+        r = r['TargetGroups'][0]
+        self.cache = unpack(r)
+        self.arn = self.cache.TargetGroupArn
+        return self.cache
+
+@inject_autokwargs(vpc=AwsVirtualPrivateCloud, lb=AwsLoadBalancer, tg=AwsLoadBalancerTargetGroup)
+class AwsLoadBalancerListener(AwsClientManaged):
+    def __init__(self, **kwargs):
+        if ('target' in kwargs) and ('targets' in kwargs):
+            raise ValueError(f"call to AwsGatewayLoadBalancer should not specify both 'target' and 'targets'")
+        elif ('target' in kwargs):
+            self.targets = [kwargs.pop('target')]
+        elif ('targets' in kwargs):
+            self.targets = kwargs.pop('targets')
+        else:
+            self.subnets = False
+        super().__init__(**kwargs)
+        self.arn = None
+
+    resource_type = 'target_group'
+    client_type = 'elbv2'
+    allproto = ['HTTP'|'HTTPS'|'TCP'|'TLS'|'UDP'|'TCP_UDP'|'GENEVE']
+
+    def do_create(self):
+        r = self.client.create_listener(
+            LoadBalancerArn=lb.arn,
+            Protocol='GENEVE',
+            Port=6081,
+            DefaultActions=[
+                {
+                'Type': 'forward',
+                'TargetGroupArn': tg.arn
+                },
+            ],
+            'ForwardConfig': {
+                'TargetGroups': [
+                    {
+                            'TargetGroupArn': 'string',
+                                'Weight': 123
+                        },
+                ],
+            }
+            Tags=[
+        )
+
+
+        r = r['Listeners'][0]
+        self.cache = unpack(r)
+        self.arn = self.cache.ListenerArn
+        return self.cache
