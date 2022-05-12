4480244ce81bda8b3128532c51b27587e3868836
Spencer Rak <srak@draper.com>
2022-05-12 19:56:35 +0000

    Add support for manageing Aws Resource Shares

---
+++ carthage_aws/ram.py
@@ -0,0 +1,48 @@
+import asyncio
+
+from carthage import *
+from carthage.dependency_injection import *
+from carthage.utils import memoproperty
+from carthage_aws.connection import AwsConnection, AwsClientManaged, run_in_executor
+from carthage_aws.network import AwsVirtualPrivateCloud, AwsSubnet
+
+from dataclasses import dataclass, field
+
+from .utils import unpack
+
+from botocore.exceptions import ClientError
+
+__all__ = ['AwsResourceShare']
+
+class AwsResourceShare(AwsClientManaged):
+    def __init__(self, **kwargs):
+        if 'share' in kwargs.keys():    
+            self.share = kwargs.pop('share')
+            if not isinstance(self.share, list):
+                self.share = [self.share]
+        if 'org' in kwargs.keys():
+            self.org = kwargs.pop('org')
+        assert hasattr(self, 'org'),f"{self} must have attribute 'org'"
+        super().__init__(**kwargs)
+        self.arn = None
+
+    resource_type = 'resource_share'
+    client_type = 'ram'
+
+    # dev-org 'arn:aws-us-gov:organizations::807241311045:organization/o-yy8ngalms6'
+    # prod-org 'arn:aws-us-gov:organizations::627530914327:organization/o-d1lk8mha82'
+
+    def do_create(self):
+        assert len(self.share) == 1
+        r = self.client.create_resource_share(
+            name=self.name,
+            resourceArns=self.share,
+            # TODO: Pull principal string from config.yml
+            principals=[self.org],
+            tags=[dict(key='Name',value=self.name)]
+        )
+        r = r['resourceShare']
+        self.cache = unpack(r)
+        self.arn = self.cache.resourceShareArn
+        return self.cache
+    
